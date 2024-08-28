9a5590f95180ca209ded6bb5f25c83740d2c06e0
IanD1013 <ian.tung1013@gmail.com>
2024-08-28 13:26:19 +1200

    Finished basic register route, start login route

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,15 +7,23 @@ const RegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function registerUser(e) {
+  async function registerUser(e) {
     e.preventDefault();
-    axios.post('/register', { name, email, password });
+
+    try {
+      await axios.post('/register', { name, email, password });
+      alert('Registeration successful. Now you can login.');
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      alert('Registeration failed. Please try again later.');
+    }
   }
 
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-32">
         <h1 className="text-4xl text-center mb-4">Register</h1>
+
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
           <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
           <input type="email" placeholder="your@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
