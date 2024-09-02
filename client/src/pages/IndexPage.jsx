4cb432261f52842fb4d8a7c7b5001eb61f798094
IanD1013 <ian.tung1013@gmail.com>
2024-09-02 21:15:19 +1200

    Add show more photos and close photos feature

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
     <div className="mt-8 gap-x-6 gap-y-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 &&
         places.map((place) => (
-          <Link to={'/place/' + place._id} key={place}>
+          <Link to={'/place/' + place._id} key={place._id}>
             <div className="bg-gray-500 mb-2 rounded-2xl">
               {place.photos?.[0] && (
                 <img className="rounded-2xl object-cover w-full h-full aspect-square" src={'http://localhost:4000/uploads/' + place.photos[0]} />
