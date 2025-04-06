d845ece05bc5b3145297c68cd91fd059e77f7acc
IanD1013 <ian.tung1013@gmail.com>
2025-04-06 21:18:32 +1200

    Remove all, start from scratch

--- client/src/components/Image.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-
-const Image = ({ src, ...rest }) => {
-  src = src && src.includes('https://') ? src : `http://localhost:4000/uploads/${src}`;
-  return <img {...rest} src={src} />;
-};
-
-export default Image;
