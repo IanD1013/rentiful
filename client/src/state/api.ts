5699a5b097bfe1008dc8b7c64bc3d92813c2e26b
IanD1013 <ian.tung1013@gmail.com>
2025-04-16 12:55:13 +1200

    Add api slice with authentication and user role-based endpoint logic

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,54 @@
+import { Manager, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+      if (idToken) {
+        headers.set("Authorization", `Bearer ${idToken}`);
+      }
+      return headers;
+    },
   }),
   reducerPath: "api",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({ // get user details from our backend 
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const userRole = idToken?.payload["custom:role"] as string;
+          const user = await getCurrentUser();
+          
+          const endpoint =
+            userRole === "manager"
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let userDetailsResponse = await fetchWithBQ(endpoint);
+
+          // if user doesn't exist, create new user in our own database 
+
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager, // all the user info get back from our backend
+              userRole,
+            },
+          };
+
+        } catch (error: any) {
+          return { error: error.message || "Could not fetch user data" };
+        }
+      }
+    })
+
+  }),
 });
 
 export const {} = api;
