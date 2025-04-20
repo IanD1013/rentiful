a2d36dab49e2bfc093c16fa907e720ec49603942
IanD1013 <ian.tung1013@gmail.com>
2025-04-20 18:35:04 +1200

    Refactor layout and navbar components to integrate authentication user data and enhance user experience

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,12 +1,20 @@
-import Navbar from '@/components/Navbar';
-import { NAVBAR_HEIGHT } from '@/lib/constants';
-import React from 'react';
+"use client";
+
+import Navbar from "@/components/Navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser } = useGetAuthUserQuery();
+
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main className="h-full flex w-full flex-col" style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+      <main
+        className="h-full flex w-full flex-col"
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
         {children}
       </main>
     </div>
