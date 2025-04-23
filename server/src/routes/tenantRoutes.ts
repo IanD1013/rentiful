e2b4316e274410997c9bc0f2f18fcd3b2694c166
IanD1013 <ian.tung1013@gmail.com>
2025-04-23 17:29:07 +1200

    Add property retrieval endpoints for managers and tenants with location formatting

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import {
-  getTenant,
   createTenant,
+  getCurrentResidences,
+  getTenant,
   updateTenant,
 } from "../controllers/tenantControllers";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
 
 export default router;
